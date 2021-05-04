7141d3acc1272f225af0ac2b8bc5c2c0027ab0f5
Bryan Lee <bryan.lee@lynxcreations.com>
2021-05-03 23:28:40 -0400

    Summary component done.

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -4,18 +4,54 @@ import { StateContext } from '../pages/Plan';
 function Summary() {
   const appState = useContext(StateContext);
 
+  function sumReducer(question) {
+    const line = {
+      borderBottom: '1px solid #0e8784',
+      paddingLeft: '60px',
+    };
+
+    switch (question) {
+      case 1:
+        if (appState.sum[0] !== '') {
+          return (
+            <span className="summary-selection">
+              {appState.sum[0] === 'Capsule' ? 'Capsules' : appState.sum[0]}
+            </span>
+          );
+        }
+        return <span style={line}> </span>;
+      case 2:
+        if (appState.sum[1] !== '') {
+          return <span className="summary-selection">{appState.sum[1]}</span>;
+        }
+        return <span style={line}> </span>;
+      case 3:
+        if (appState.sum[2] !== '') {
+          return <span className="summary-selection">{appState.sum[2]}</span>;
+        }
+        return <span style={line}> </span>;
+      case 4:
+        return <span className="summary-selection">{appState.sum[3]}</span>;
+
+      case 5:
+        if (appState.sum[4] !== '') {
+          return <span className="summary-selection">{appState.sum[4]}</span>;
+        }
+        return <span style={line}> </span>;
+      default:
+        break;
+    }
+  }
   return (
     <div className="summary-bg u-border-radius">
       <div className="summary-container">
         <h5 className="summary-heading">Order Summary</h5>
         <p className="heading-quaternary">
           "I drink my coffee {appState.sum[0] === 'Capsule' ? 'using ' : 'as '}
-          <span>
-            {appState.sum[0] === 'Capsule' ? 'Capsules' : appState.sum[0]}
-          </span>
-          , with a <span>{appState.sum[1]}</span> type of bean.{' '}
-          <span>{appState.sum[2]}, </span>
-          sent to me <span>{appState.sum[4]}</span>"
+          {sumReducer(1)}, with a {sumReducer(2)} type of bean. {sumReducer(3)}
+          {' , '}
+          {appState.sum[3] !== '' ? 'ground ala ' : null}
+          {sumReducer(4)} sent to me {sumReducer(5)}."
         </p>
       </div>
     </div>
